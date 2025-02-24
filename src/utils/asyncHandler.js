ea9359b6ce7d89277811e9918ced3d4bda48ee86
Samir-Tripura <samirtripura77@gmail.com>
2025-02-24 14:35:02 +0530

    error handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,12 @@
+const asyncHandler = (resquestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(resquestHandler (req, res, next)).
+        catch((err) => next(err))
+
+
+    }
+}
+
+
+
+export {asyncHandler}
